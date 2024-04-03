dfd058d3e2f0b190da649d43d51923c9d228866d
JoaoVellenich <jpvellenich@gmail.com>
2024-04-03 14:49:43 -0300

    feat: adding order structure to project

---
+++ consumer/src/main/java/com/joaovellenich/consumer/data/OrderData.java
@@ -0,0 +1,17 @@
+package com.joaovellenich.consumer.data;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderData {
+    private String id;
+    private String ProductName;
+    private BigDecimal value;
+}
